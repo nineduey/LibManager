b130dacb2f71923374a839eb4405e4543d7c1716
Amanda Todakonzie <vooshpanda@gmail.com>
2021-11-26 22:27:48 -0800

    change while loop to import data

--- librarymanager.cpp
+++ librarymanager.cpp
@@ -9,35 +9,16 @@ class HashMap;
 
 void LibraryManager::readInventory(ifstream& inFile){
 
-    Book* temp;
+    	 Book* temp;
 	 char bookType;
-	 string author;
-	 string title;
-	 int month;
-	 int year;
 
 
 	 while (!inFile.eof()) {
 
 		  inFile >> bookType;
+		  temp = bf.createBook(bookType);
 		  inFile.get();
-		  if (bookType == 'P') { //if peridical?
-				getline(inFile, title, ',');
-				inFile >> month;
-				inFile >> year;
-				temp = bf.createBook(bookType);
-				temp->setData(author, month, year);
-				
-
-		  }
-		  else {
-				getline(inFile, author, ',');
-				inFile.get();
-				getline(inFile, title, ',');
-				inFile >> year;
-				temp = bf.createBook(bookType);
-				temp->setData(author, title, year);
-		  }	 	  
+		  temp.setData(inFile);
 	 }
 }
 
