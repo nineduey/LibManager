f9195b15795a932c2ae3f69c52d10729631821e9
Amanda Todakonzie <vooshpanda@gmail.com>
2021-11-26 22:25:14 -0800

    Updated readInventory()

--- librarymanager.cpp
+++ librarymanager.cpp
@@ -1,1 +1,51 @@
 #include "librarymanager.h"
+
+
+class Book;
+class BookFactory;
+class TransactionFactory;
+class HashMap;
+
+
+void LibraryManager::readInventory(ifstream& inFile){
+
+    Book* temp;
+	 char bookType;
+	 string author;
+	 string title;
+	 int month;
+	 int year;
+
+
+	 while (!inFile.eof()) {
+
+		  inFile >> bookType;
+		  inFile.get();
+		  if (bookType == 'P') { //if peridical?
+				getline(inFile, title, ',');
+				inFile >> month;
+				inFile >> year;
+				temp = bf.createBook(bookType);
+				temp->setData(author, month, year);
+				
+
+		  }
+		  else {
+				getline(inFile, author, ',');
+				inFile.get();
+				getline(inFile, title, ',');
+				inFile >> year;
+				temp = bf.createBook(bookType);
+				temp->setData(author, title, year);
+		  }	 	  
+	 }
+}
+
+void LibraryManager::readCustomer(ifstream& inFile) {
+
+}
+
+
+void LibraryManager::readTransaction(ifstream& inFile) {
+
+}
