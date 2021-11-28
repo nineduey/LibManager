ae2b51a8f6df74fcb8f30e6e5cee6d0771fb0f2c
Amanda Todakonzie <vooshpanda@gmail.com>
2021-11-28 15:40:14 -0800

    Amanda update to item.cpp
    Added all function definitions
    Added print() function to be called in ostream operator<<

--- item.cpp
+++ item.cpp
@@ -1,9 +1,71 @@
+/*
+@File contents: Item.cpp function definitions
+
+@Purpose:
+
+@Assumptions:
+
+@Authors: Shushmitha Radjaram and Amanda Todakonzie
+
+@How code is used:
+*/
+//-----------------------------------------------------------------------------
 #include "item.h"
 
-Item::Item()
-{
+
+// operator << :
+ostream& operator<<(ostream& out, const Item& anItem) {
+	 anItem.print(out);
+	 return out;
 }
 
-Item::~Item()
-{
+//---------------------------------------------------------------------------
+//Default Constrcutor
+Item::Item() {
+	 left = nullptr;
+   right = nullptr;
+	 numInLib = 0;
+	 itemType = 'I';
+}
+
+//----------------------------------------------------------------------------
+// Destructor
+Item::~Item() {
+	 left = nullptr;
+	 right = nullptr;
+}
+
+
+//----------------------------------------------------------------------------
+// checkOut() : method to check out a book from the library
+void Item::checkOut() {
+	 if (numInLib > 0) {
+		  numInLib--;
+		  return;
+	 }
+	 cout << "Error: Cannot check out, no more copies available." << endl;
+}
+
+
+//----------------------------------------------------------------------------
+// checkIn() : method to check in a book from the library
+void Item::checkIn() {
+	 if (numInLib < maxNumInLib) {
+		  numInLib++;
+		  return;
+	 }
+	 cout << "Error: Cannot check in, no room in Library." << endl;
+}
+
+//----------------------------------------------------------------------------
+// print() : method to call virtual print function for ostream operator<<
+void Item::print(ostream& os) const {
+	 this->print(os);
+}
+
+//----------------------------------------------------------------------------
+// validate() : method to check the input data being set as data members of 
+// Item object class
+bool Item::validate() const {
+
 }
