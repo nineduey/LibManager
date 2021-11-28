bf00a9491b0711a26bb012d9f94fe637c89c70ab
Amanda Todakonzie <vooshpanda@gmail.com>
2021-11-28 15:41:37 -0800

    Amanda update to item.cpp
    Removed tabs

--- item.cpp
+++ item.cpp
@@ -15,52 +15,52 @@
 
 // operator << :
 ostream& operator<<(ostream& out, const Item& anItem) {
-	 anItem.print(out);
-	 return out;
+    anItem.print(out);
+    return out;
 }
 
 //---------------------------------------------------------------------------
 //Default Constrcutor
 Item::Item() {
-	 left = nullptr;
-   right = nullptr;
-	 numInLib = 0;
-	 itemType = 'I';
+    left = nullptr;
+    right = nullptr;
+    numInLib = 0;
+    itemType = 'I';
 }
 
 //----------------------------------------------------------------------------
 // Destructor
 Item::~Item() {
-	 left = nullptr;
-	 right = nullptr;
+    left = nullptr;
+    right = nullptr;
 }
 
 
 //----------------------------------------------------------------------------
 // checkOut() : method to check out a book from the library
 void Item::checkOut() {
-	 if (numInLib > 0) {
-		  numInLib--;
-		  return;
-	 }
-	 cout << "Error: Cannot check out, no more copies available." << endl;
+    if (numInLib > 0) {
+        numInLib--;
+        return;
+    }
+    cout << "Error: Cannot check out, no more copies available." << endl;
 }
 
 
 //----------------------------------------------------------------------------
 // checkIn() : method to check in a book from the library
 void Item::checkIn() {
-	 if (numInLib < maxNumInLib) {
-		  numInLib++;
-		  return;
-	 }
-	 cout << "Error: Cannot check in, no room in Library." << endl;
+    if (numInLib < maxNumInLib) {
+        numInLib++;
+        return;
+    }
+    cout << "Error: Cannot check in, no room in Library." << endl;
 }
 
 //----------------------------------------------------------------------------
 // print() : method to call virtual print function for ostream operator<<
 void Item::print(ostream& os) const {
-	 this->print(os);
+    this->print(os);
 }
 
 //----------------------------------------------------------------------------
