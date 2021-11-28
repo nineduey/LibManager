c2a7c81ce6d4356d4e011ed53ea597c0772694a2
Amanda Todakonzie <vooshpanda@gmail.com>
2021-11-28 09:31:00 -0800

    Added virtual print()

--- fiction.h
+++ fiction.h
@@ -1,3 +1,11 @@
+/*
+@File contents: Fiction class function definitions
+@Purpose:
+@Assumptions:
+@Authors: Shushmitha Radjaram and Amanda Todakonzie
+@How code is used:
+*/
+//-----------------------------------------------------------------------------
 #ifndef FICTION_H
 #define FICTION_H
 
@@ -6,30 +14,27 @@
 #include "book.h"
 using namespace std;
 
-class Fiction : public Book
-{
-    friend ostream& operator<<( ostream&, const Item& );
+class Fiction : public Book {
+
+    friend ostream& operator<<(ostream&, const Item&);
 
 private:
 
     string author;
-    
     //consts for output (TODO: put in a dictionary)
     //const string fictionBookDeclar = "FICTION BOOK";
-
-    const int AUTHOR_SPACE_LENGTH = 9; //size of the space after author output
-    const int TITLE_SPACE_LENGTH = 9;  //size of the space after title output
-
+    
 public:
     Fiction();
     ~Fiction();
-    virtual Item& operator=( const Item& );
-    virtual bool operator==( const Item& ) const;
-    virtual bool operator!=( const Item& ) const;
-    virtual bool operator<( const Item& ) const;
-    virtual bool operator>( const Item& ) const;
+    virtual Item& operator=(const Item&);
+    virtual bool operator==(const Item&);
+    virtual bool operator!=(const Item&);
+    virtual bool operator<(const Item&);
+    virtual bool operator>(const Item&);
     virtual Item* create() const;
-    virtual void setData( istream& );
+    virtual void setData(istream&);
+    virtual void print(ostream& os) const;
 };
 
 #endif
