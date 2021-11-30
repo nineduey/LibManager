ad0cd9158cfa86d1ecd47040aa7a4d82c3fc1d8d
Amanda Todakonzie <vooshpanda@gmail.com>
2021-11-30 12:31:19 -0800

    Update 11/30 Tues morning

--- book.h
+++ book.h
@@ -19,51 +19,19 @@
 #include <string>
 using namespace std;
 
-//global constants for output
-const int AUTHOR_SPACE_LENGTH = 10; //size of the space after author output
-const int TITLE_SPACE_LENGTH = 10;  //size of the space after title output
-const int MONTH_SPACE_LENGTH = 10; //size of the space after month output
-const int YEAR_SPACE_LENGTH = 10;  //size of the space after year output
-
-<<<<<<< Updated upstream
 //global constants to be used in derived classes
 const int AVAILABILITY_LENGTH = 6;
 const int AUTHOR_LENGTH = 9; // size of the space after author output
 const int TITLE_LENGTH = 9; // size of the space after title output
 
 
-class Book : public Item
-{
-    friend ostream& operator<<( ostream&, const Item& );
-=======
-// global constants
-const int AUTHOR_SPACE_LENGTH = 9; //size of the space after author output
-const int TITLE_SPACE_LENGTH = 9;  //size of the space after title output
-const int MONTH_SPACE_LENGTH = 4; //size of the space after month output
-const int YEAR_SPACE_LENGTH = 4;  //size of the space after year output
-
 class Book: public Item {
 
     friend ostream& operator<<(ostream&, const Book&);
->>>>>>> Stashed changes
 
 public:
     Book();
     ~Book();
-
-    virtual Item& operator=(const Item&) const = 0;
-    virtual bool operator==(const Item&) const = 0;
-    virtual bool operator!=(const Item&) const = 0;
-    virtual bool operator<(const Item&) const = 0;
-    virtual bool operator>(const Item&) const = 0;
-    virtual Item* create() const = 0;
-<<<<<<< Updated upstream
-    virtual void setData( istream& ) = 0;
-=======
-    virtual void setData(istream&) const = 0;
->>>>>>> Stashed changes
-    virtual void print(ostream& os) const;
-
     virtual char returnItemType() const = 0;
     virtual char returnItemType_Genre() const = 0;
     virtual Item* create() const = 0;
