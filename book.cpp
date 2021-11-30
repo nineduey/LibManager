9987ea0f672c648c4cf5b5f5741f26d22bee5e6d
Amanda Todakonzie <vooshpanda@gmail.com>
2021-11-30 12:30:03 -0800

    Update 11/30 Tues morning

--- book.cpp
+++ book.cpp
@@ -14,29 +14,18 @@
 #include "book.h"
 
 ostream& operator<<(ostream& out, const Book& aBook) {
-<<<<<<< Updated upstream
     aBook.print(out);
     return out;
-=======
-	 aBook.print(out);
-	 return out;
->>>>>>> Stashed changes
 }
 
 //----------------------------------------------------------------------------
 // Default Constructor: sets default values when creating a new instance
 Book::Book() {
-<<<<<<< Updated upstream
     title = "";
     year = 1000;
     bookType = 'B';
     itemType = 'I';
-=======
-	 title = "";
-	 year = 1000;
-    bookType = 'B';
-	 itemType = 'I';
->>>>>>> Stashed changes
+
 }
 
 //----------------------------------------------------------------------------
@@ -45,34 +34,7 @@ Book::~Book() {}
 
 //----------------------------------------------------------------------------
 // print() : Virtual print function, will display data members of Book object
-void Book::print(ostream& os) const {
-<<<<<<< Updated upstream
-    this->print(os);
-=======
-	 this->print(os);
->>>>>>> Stashed changes
-@File contents: Book class function definitions
-
-@Purpose:
-
-@Assumptions:
-
-@Authors: Shushmitha Radjaram and Amanda Todakonzie
-
-@How code is used:
-*/
-//-----------------------------------------------------------------------------
-#include "book.h"
-
-//----------------------------------------------------------------------------
-// Default Constructor
-Book::Book()
+void Book::print(ostream& os) const 
 {
-	 title = "";
-	 year = 0000;
-	 bookType = 'B';
+    this->print(os);
 }
-
-//----------------------------------------------------------------------------
-// Destructor
-Book::~Book() {}
