2d8e5f928548dc2ac202b2d3dff551dd111a6c79
Amanda Todakonzie <vooshpanda@gmail.com>
2021-11-30 12:37:51 -0800

    Update 11/30 Tues Morning

--- item.cpp
+++ item.cpp
@@ -12,30 +12,28 @@
 //-----------------------------------------------------------------------------
 #include "item.h"
 
-<<<<<<< Updated upstream
-//Default Constrcutor
-Item::Item() {
-	 left = nullptr;
-   right = nullptr;
-=======
 
+//-----------------------------------------------------------------------------
+// ostream operator << : displays Item contentsm uses virtuak print()
+// @pre: must be passed a referenced ostreeam obj and a referenced Item obj
+// @post: calls on the virtual print() method, particular to its defined
+// derived class
+ostream& operator<<(ostream& out, const Item& anItem) {
+	 anItem.print(out);
+	 return out;
+}
 
 //-----------------------------------------------------------------------------
 // Default Constrcutor: method to initialize private & protected data members
 Item::Item() {
 	 left = nullptr;
-    right = nullptr;
->>>>>>> Stashed changes
+	 right = nullptr;
 	 numInLib = 0;
 	 maxNumInLib = 5;
 	 itemType = 'I';
 }
 
 //----------------------------------------------------------------------------
-<<<<<<< Updated upstream
-// Destructor
-=======
->>>>>>> Stashed changes
 // Destructor: sets the left and right Item pointers to null
 Item::~Item() {
 	 left = nullptr;
@@ -44,10 +42,6 @@ Item::~Item() {
 
 
 //----------------------------------------------------------------------------
-<<<<<<< Updated upstream
-// checkOut() : method to check out a book from the library
-=======
->>>>>>> Stashed changes
 // checkOut(): method to check out an Item contained in Library catalogue
 // @pre:
 // @post:
@@ -61,10 +55,6 @@ void Item::checkOut() {
 
 
 //----------------------------------------------------------------------------
-<<<<<<< Updated upstream
-// checkIn() : method to check in a book from the library
-=======
->>>>>>> Stashed changes
 // checkIn(): method to check out an Item contained in Library catalogue
 // @pre:
 // @post:
@@ -76,39 +66,15 @@ void Item::checkIn() {
 	 cout << "Error: Cannot check in, no room in Library." << endl;
 }
 
-
-<<<<<<< Updated upstream
-//----------------------------------------------------------------------------  should this be virtual?
-// returnItemType() : method to return the type of object being held within 
-// the item class
-=======
-//----------------------------------------------------------------------------
->>>>>>> Stashed changes
-char Item::returnItemType() const {
-	 return itemType;
-}
-
-
-//----------------------------------------------------------------------------
-<<<<<<< Updated upstream
-// validate() : method to check the input data being set as data members of 
-// Item object class
-=======
->>>>>>> Stashed changes
 //----------------------------------------------------------------------------
 // validate(): method to validate contained within object is valid
 // @pre:
 // @post:
 bool Item::validate() const {
-
+	 if (numInLib >= 0 && numInLib <= maxNumInLib) {
+		  return true;
+	 }
+	 return false;
 }
 
 
-ostream& operator<<(ostream& out, const Item& anItem) {
-	 anItem.print(out);
-	 return out;
-<<<<<<< Updated upstream
-}
-=======
-}
->>>>>>> Stashed changes
