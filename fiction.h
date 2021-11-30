3a638e32d3fdc97e3bd5e9d78129efdd6d337853
Amanda Todakonzie <vooshpanda@gmail.com>
2021-11-30 12:34:46 -0800

    Update 11/30 Tues morning

--- fiction.h
+++ fiction.h
@@ -1,8 +1,4 @@
 /*
-@File contents: Fiction class function definitions
-@Purpose:
-@Assumptions:
-@Authors: Shushmitha Radjaram and Amanda Todakonzie
 @File contents: Fiction function and variable declarations
 
 @Purpose:
@@ -11,7 +7,6 @@
 
 @Authors: Shushmitha Radjaram and Amanda Todakonzie
 
->>>>>>> Stashed changes
 @How code is used:
 */
 //-----------------------------------------------------------------------------
@@ -23,40 +18,9 @@
 #include "book.h"
 using namespace std;
 
-class Fiction : public Book {
-
-    friend ostream& operator<<(ostream&, const Item&);
-
-private:
-
-    string author;
-<<<<<<< Updated upstream
-    //consts for output (TODO: put in a dictionary)
 class Fiction : public Book
 {
     //const string fictionBookDeclar = "FICTION BOOK";
-    
-=======
-
-    //consts for output (TODO: put in a dictionary)
-    //const string fictionBookDeclar = "FICTION BOOK";
-    
-
->>>>>>> Stashed changes
-public:
-    Fiction();
-    ~Fiction();
-    virtual Item& operator=(const Item&);
-    virtual bool operator==(const Item&);
-    virtual bool operator!=(const Item&);
-    virtual bool operator<(const Item&);
-    virtual bool operator>(const Item&);
-    virtual Item* create() const;
-    virtual void setData(istream&);
-    virtual void print(ostream& os) const;
-<<<<<<< Updated upstream
-=======
-
 
 public:
     Fiction();
@@ -77,7 +41,6 @@ public:
 private:
     string author;
 
->>>>>>> Stashed changes
 };
 
 #endif
