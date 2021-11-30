2fc712ae244e560fadda443a5d9229d16d46b9ae
Amanda Todakonzie <vooshpanda@gmail.com>
2021-11-30 12:36:14 -0800

    Update 11/30 Tues morning

--- item.h
+++ item.h
@@ -1,13 +1,10 @@
 /*
-@File contents: BinTree class function and variable declarations
-
-@Purpose: This is the class will take the place of Nodes that will be stored 
 @File contents: Item class function and variable declarations
 
 @Purpose: This is the class will take the place of Nodes that will be stored
 in the BinTree class. The Items that held in the BinClass will hold a NodeData
-pointer which for this assignment will point to a book. This design is extensible, 
-such as adding another class called Music, the NodeData objects could point to a 
+pointer which for this assignment will point to a book. This design is extensible,
+such as adding another class called Music, the NodeData objects could point to a
 Music object instead of a Book object. This allows different BinTrees
 to be built, holding specific kinds of Node Data derived classes
 
@@ -22,49 +19,22 @@ to be built, holding specific kinds of Node Data derived classes
 #define ITEM_H
 
 #include <iostream>
-#include "bintree.h"
 #include <string>
+#include <iostream>
 
 using namespace std;
 
-//forward referencing - not needed?
+//forward referencing
 //class Book;
 
-class Item {
-
-	 friend class BinTree;
-	 friend ostream& operator<<(ostream&, const Item&);
+class Item
+{
+	friend class BinTree;
+	//friend class ostream;
+	friend ostream& operator<<( ostream&, const Item& );
 
 public:
 
-	 Item(); // default constructor
-	 virtual ~Item(); // destructor
-	 void checkOut();
-	 void checkIn();
-	 char returnItemType() const;
-	 virtual Item* create() const = 0;
-	 virtual void setData(ifstream&) const = 0;
-	 virtual void print(ostream& os) const;
-
-	 virtual Item& operator=(const Item&) const = 0;
-	 virtual bool operator==(const Item&) const = 0;
-	 virtual bool operator!=(const Item&) const = 0;
-	 virtual bool operator<(const Item&) const = 0;
-	 virtual bool operator>(const Item&) const = 0;
-
-private:
-	 Item* left;
-	 Item* right;
-
-protected: 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-	 int numInLib; // number of current copies of item in library
-	 int maxNumInLib; // max number of copies of item in library - QUESTION** : Should we make this a constant?
-	 char itemType;	// added this data member to store the type of item the instance is
-	 bool validate() const; // check whether the input content is valid
 	Item();			  // default constructor
 	virtual ~Item(); // destructor
 	void checkOut(); // check out an Item from the BinTree
