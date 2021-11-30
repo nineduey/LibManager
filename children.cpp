3fe9fb6320cd1b3f5e79f6cf04df3d728f65ab10
Amanda Todakonzie <vooshpanda@gmail.com>
2021-11-30 12:34:14 -0800

    Update 11/30 Tues morning

--- children.cpp
+++ children.cpp
@@ -13,12 +13,6 @@
 #include "children.h"
 #include <iomanip>
 
-// sorted by title, then author
-//----------------------------------------------------------------------------
-// Default Constructor
-Children::Children()
-{
-    author = "";
 //sorted by title, then author
 
 
@@ -30,44 +24,6 @@ Children::Children()
 
 //----------------------------------------------------------------------------
 // Destructor
-<<<<<<< Updated upstream
-Children::~Children(){}
-=======
-Children::~Children() {}
->>>>>>> Stashed changes
-
-//----------------------------------------------------------------------------
-// operator = : Assignment operator, creates deep copy of the Item& anItem
-// passed in
-// @pre:
-//@post:
-Item& Children::operator=(const Item& item) {
-    const Children& aChildren = static_cast<const Children&>(item);
-
-    this->title = aChildren.title;
-    this->year = aChildren.year;
-    this->author = aChildren.author;
-    this->numInLib = aChildren.numInLib;
-
-    return *this;
-}
-
-//----------------------------------------------------------------------------
-//operator == : 
-//@pre:
-//@post:
-bool Children::operator==(const Item& item) {
-    const Children& aChildren = static_cast<const Children&>(item);
-
-    return (this->title == aChildren.title && this->author == aChildren.author);
-}
-
-//----------------------------------------------------------------------------
-//operator == : 
-//@pre:
-//@post:
-bool Children::operator!=(const Item& item) {
-    return !this->operator==(item);
 Children::~Children() {}
 
 //----------------------------------------------------------------------------
@@ -109,95 +65,6 @@ Item& Children::operator=( const Item& item)
 //operator == : 
 //@pre:
 //@post:
-bool Children::operator<(const Item& item) {
-    if (this->operator==(item)) {
-        return false;
-    }
-
-    const Children& aChildren = static_cast<const Children&>(item);
-
-    if (this->title < aChildren.title) {
-        return true;
-    }
-    else if (this->title == aChildren.title) {
-        return this->author < aChildren.author;
-    }
-}
-
-//----------------------------------------------------------------------------
-//operator == : 
-//@pre:
-//@post:
-bool Children::operator>(const Item& item) {
-    if (this->operator==(item) || this->operator<(item)) {
-        return false;
-    }
-
-    const Children& aChildren = static_cast<const Children&>(item);
-
-    if (this->title > aChildren.title) {
-        return true;
-<<<<<<< Updated upstream
-    } 
-=======
-    }
->>>>>>> Stashed changes
-    else if (this->title == aChildren.title) {
-        return this->author > aChildren.author;
-    }
-}
-
-//----------------------------------------------------------------------------
-//create() : 
-//@pre:
-//@post:
-Item* Children::create() const {
-<<<<<<< Updated upstream
-	 return new Children;
-=======
-    return new Children;
->>>>>>> Stashed changes
-}
-
-//----------------------------------------------------------------------------
-//setData(): 
-//@pre:
-//@post:
-void Children::setData(istream& infile) {
-    getline(infile, author, ',');     // input author, looks for comma terminator
-
-    /*infile.get();
-    getline( infile, authorLast, ',' );*/
-
-    infile.get();                // get (and ignore) blank before title
-    getline(infile, title, ','); // input title
-    infile >> year;              // input year
-
-    itemType = 'B';     // setting itemType -> Item class
-    numInLib = 5;          // setting numer of Book copies -> Item class
-    bookType = 'C';        // setting bookType -> Book class
-<<<<<<< Updated upstream
-    
-}
-
-//----------------------------------------------------------------------------
-ostream& operator<<(ostream& out, const Children& aChildrenBook) {
-	 aChildrenBook.print(out);
-	 return out;
-}
-
-//----------------------------------------------------------------------------
-void Children::print(ostream& os) const {
-	 cout << numInLib << "     " << author << "     " << title << "     " << year;
-=======
-
-}
-
-//----------------------------------------------------------------------------
-ostream& operator<<(ostream& out, const Children& aChildrenBook) {
-    aChildrenBook.print(out);
-    return out;
->>>>>>> Stashed changes
 bool Children::operator==( const Item& item) const
 {
 	const Children& aChildren = static_cast<const Children&>(item);
@@ -294,9 +161,4 @@ void Children::setData( istream& infile)
 void Children::print( ostream& os ) const
 {
 	os << numInLib << "     " << author << "     " << title << "     " << year;
-}
-
-//----------------------------------------------------------------------------
-void Children::print(ostream& os) const {
-    cout << numInLib << "     " << author << "     " << title << "     " << year;
 }
