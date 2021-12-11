e860ecaeabfcd6d3adbd9b92dbc6d8a14a689aa1
Amanda Todakonzie <vooshpanda@gmail.com>
2021-12-10 21:11:25 -0800

    FINAL EDIT: final comments & formatting

--- item.h
+++ item.h
@@ -1,14 +1,17 @@
 /*
-@File contents: Item class function and variable declarations
-@Purpose: This is the class will take the place of Nodes that will be stored
-in the BinTree class. The Items that held in the BinClass will hold a NodeData
-pointer which for this assignment will point to a book. This design is extensible,
-such as adding another class called Music, the NodeData objects could point to a
-Music object instead of a Book object. This allows different BinTrees
-to be built, holding specific kinds of Node Data derived classes
-@Assumptions:
+@File contents: Function and variable declarations for Item Class
+
+@Purpose: The Item class serves as an abstract class for its derived classes,
+such as Book and Book's derived classes. Item pointers are stored inside of the
+container BinTree Class's nodes.
+
+@Assumptions: Its derived classes of Book, and Book's derived classes of
+Childre, Fiction, and Periodical are defined as well as its containter BinTree
+
 @Authors: Shushmitha Radjaram and Amanda Todakonzie
-@How code is used:
+
+@How code is used: Item objects are instantiated withint the FacDriver class to
+create new empty objects of its derived classes that are defined.
 */
 //-----------------------------------------------------------------------------
 #ifndef ITEM_H
@@ -21,21 +24,24 @@ using namespace std;
 class Item
 {
 	friend class BinTree;
+	// ostream operators used for printing
 	friend ostream& operator<<( ostream&, const Item& );
 
 public:
 
-	Item();			  // default constructor
+	Item();		 // default constructor
 	virtual ~Item(); // destructor
 	bool checkOut(); // check out an Item from the BinTree
 	bool checkIn();  // check in an Item from the BinTree
+	
+	//virtual functions
 	virtual char returnItemType() const = 0; // returns ItemType
-	virtual char returnItemType_Genre() const = 0;// returns the Type of ItemType
-	virtual Item* create() const = 0;		  // virtual create function
-	virtual bool setData( istream& ) = 0;	  // virtual setData function
-	virtual bool setDataInput( istream& ) = 0;	  // virtual setData function
+	virtual char returnItemType_Genre() const = 0;// returns genre of ItemType
+	virtual Item* create() const = 0; // virtual create function
+	virtual bool setData( istream& ) = 0; // virtual setData function
+	virtual bool setDataInput( istream& ) = 0; // virtual setData function
 	virtual void print( ostream& os ) const = 0;// virtual print function
-	virtual void printHeader() const = 0;  // virtual print header function
+	virtual void printHeader() const = 0; // virtual print header function
 	virtual void printKeyInfo() const = 0;
 
 	//virtual comparison operators
@@ -45,13 +51,12 @@ public:
 	virtual bool operator<( const Item& ) const = 0;
 	virtual bool operator>( const Item& ) const = 0;
 
-
 protected:
 	int numInLib; // number of current copies of item in library
 	int maxNumInLib; // max number of copies of item in library
-	char itemType;	 // added this data member to store the type of item the instance is
-	char itemFormats[5];
-	char itemFormat;
+	char itemType;	 // stores the type of item the instance is
+	char itemFormats[5]; //array of valid format types
+	char itemFormat; // the format of the particular Item object instance
 };
 
 #endif
