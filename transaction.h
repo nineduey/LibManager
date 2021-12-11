42832d863994128ba34c65cad8fe0875c4438e51
Amanda Todakonzie <vooshpanda@gmail.com>
2021-12-10 20:55:57 -0800

    FINAL EDIT: final comments & formatting

--- transaction.h
+++ transaction.h
@@ -1,11 +1,18 @@
 /*
-@File contents: Function and variable declarations
-of the Transaction class
-@Purpose:
-@Assumptions
+@File contents: Function and variable declarations of the Transaction class
+
+@Purpose: Transaction class serves as an abstract class for the derived classes
+that handle commands being read into the program from the readTransaction()
+method of the libraryManager class. Derived classes include Checkout, Return,
+PatronHistory and DisplayCatalogue. Each derived class handles a transaction
+process particular to its own class.
+
+@Assumptions:  Derived classes of the Transaction class are defined.
+
 @Authors: Shushmitha Radjaram and Amanda Todakonzie
-@How code is used:
 */
+//-----------------------------------------------------------------------------
+
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 #include <fstream>
@@ -21,10 +28,10 @@ class Transaction
 public:
     Transaction(); // default constructor
     virtual ~Transaction(); // virtual destructor
-    virtual Transaction* create() const = 0; //virtual create method to return a pointer to the certain transaction type object
+    virtual Transaction* create() const = 0; // virtual create method
     virtual bool setData( istream& ) = 0; // virtual set data method
-    virtual void doTransaction( Storage&, HashMap& ) = 0;   // performs whichever transaction based on the type of derived class created
+    // virtual doTransaction method, defined in derived classes
+    virtual void doTransaction( Storage&, HashMap& ) = 0;  
 };
 
 #endif
-
